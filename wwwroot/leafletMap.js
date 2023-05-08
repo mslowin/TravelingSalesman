65925a48fc84974050f508c04b3b553491be8ea6
Michał Słowiński <62020662+mslowin@users.noreply.github.com>
2023-05-08 10:16:17 +0200

    Added basic Leaflet Map

---
+++ wwwroot/leafletMap.js
@@ -0,0 +1,5 @@
+﻿export function load_map() {
+    let map = L.map('map').setView({ lon: 0, lat: 0 }, 2);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 19 }).addTo(map);
+    return "";
+}
